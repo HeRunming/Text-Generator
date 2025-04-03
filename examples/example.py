2040366a4641fc91d7ca492afacecec8aa16f7e9
wongzhenhao <zhenhao1141@gmail.com>
2025-04-03 14:13:36 +0800

    add reasoning generator

--- examples/example.py
+++ examples/example.py
@@ -1,6 +1,20 @@
 import sys
-sys.path.append("/root/workspace/culfjk4p420c73amv510/herunming/Text-Generator")
+import logging
+sys.path.append("..")
+sys.path.append(".")
+import yaml
+from algorithms.AnswerGenerater_reasoning import AnswerGenerater_reasoning
+logging.basicConfig(level=logging.INFO)
 
-import algorithms.formatexample as formatexample
+def main():
+    with open("../configs/reasoning.yaml", "r") as f:
+        config = yaml.safe_load(f)
+    
+    algorithm_name = config['algorithm']
+    configs = config['configs']
+    algorithm = AnswerGenerater_reasoning(configs)
+    algorithm.run()
+    
 
-example = formatexample.Formatexample(config_path="/root/workspace/culfjk4p420c73amv510/herunming/Text-Generator/configs/example.yaml")
+if __name__ == "__main__":
+    main()
